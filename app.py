472969c4e3c049561564920dcab3593a256fdbb5
林逸聪 <lyc@meitu.com>
2016-10-05 16:53:56 +0800

    rename some file

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ app = Flask(__name__, template_folder='templates', static_folder='files')
 app.debug = True
 # Create dummy secrey key so we can use flash
 app.config['SECRET_KEY'] = '123456790'
-app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+app.config['DATABASE_FILE'] = 'user_db.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
